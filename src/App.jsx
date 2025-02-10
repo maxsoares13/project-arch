2b5d0d4b0cd0f03227474632ecf905f0442784d0
maxsoares13 <carnage.max@gmail.com>
2025-02-10 01:16:20 -0300

    *adjusting_routes*

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
+
 import './main.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<><h1>HOME</h1></>}></Route>
+          <Route path="/about" element={<><h1>ABOUT</h1><Link to="/">Navegar</Link></>}></Route>
+          <Route path="/projects" element={<><h1>PROJECTS</h1></>}></Route>
+          <Route path="/contact" element={<><h1>CONTACT</h1></>}></Route>
+        </Routes>
+      </Router>
     </>
   )
 }
