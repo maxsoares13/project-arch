832ea8039d8b1c88816973cddb29140b4234599c
maxsoares13 <carnage.max@gmail.com>
2025-02-10 01:52:31 -0300

    *add_pages*

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom"
 
 import './main.css'
 
+// PAGES
+import Home from "./pages/Home"
+import About from "./pages/About"
+import Projects from "./pages/Projects"
+import Contact from "./pages/Contact"
+
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<><h1>HOME</h1></>}></Route>
-          <Route path="/about" element={<><h1>ABOUT</h1><Link to="/">Navegar</Link></>}></Route>
-          <Route path="/projects" element={<><h1>PROJECTS</h1></>}></Route>
-          <Route path="/contact" element={<><h1>CONTACT</h1></>}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </Router>
     </>
